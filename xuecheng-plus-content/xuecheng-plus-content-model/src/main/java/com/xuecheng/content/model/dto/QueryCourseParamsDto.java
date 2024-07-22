61cb034dfc9fb28f94a6f61a9a9b2ddb805643e5
cmn <1195903721@qq.com>
2024-07-22 22:22:41 +0800

    课程查询接口的初步实现

---
+++ xuecheng-plus-content/xuecheng-plus-content-model/src/main/java/com/xuecheng/content/model/dto/QueryCourseParamsDto.java
@@ -0,0 +1,21 @@
+package com.xuecheng.content.model.dto;
+/**
+ * TODO 课程查询条件模型类
+ */
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class QueryCourseParamsDto {
+
+    //审核状态
+    private String auditStatus;
+    //课程名称
+    private String courseName;
+    //发布状态
+    private String publishStatus;
+
+}
+
